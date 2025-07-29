4ae1213976086230e4589a7a5fd7d8ad5e992a5f
kiran2396 <kirankumarca966@gmail.com>
2025-07-29 14:27:32 +0530

    version 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : '/CICD-FIRST/',
+  base: '/CICD-First/',
   plugins: [react()],
 })
